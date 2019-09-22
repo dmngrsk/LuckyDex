0cfbb57abfc1745603404f37d0081a48b48ae7ae
dmngrsk <me@dmngrsk.pl>
2019-09-22 17:40:34 +0200

    Add cookies endpoint for development purposes

---
+++ src/LuckyDex.Front/Controllers/LuckyDexCookiesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LuckyDex.Front.Controllers
+{
+    [Route("api/cookies")]
+    public class LuckyDexCookiesController : Controller
+    {
+        [HttpGet("add-cookie/{name}")]
+        public ActionResult<string> AddCookie(string name)
+        {
+            Response.Cookies.Append(name, "1");
+
+            return Ok();
+        }
+    }
+}
