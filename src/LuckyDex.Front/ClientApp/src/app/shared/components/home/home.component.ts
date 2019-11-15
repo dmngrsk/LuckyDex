3551ab0dc57342b94e3028597132ac3b1370cfa6
dmngrsk <me@dmngrsk.pl>
2019-11-16 00:35:56 +0100

    Add hidden Map information on home page

--- src/LuckyDex.Front/ClientApp/src/app/shared/components/home/home.component.ts
+++ src/LuckyDex.Front/ClientApp/src/app/shared/components/home/home.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { CookieReadingService } from '../../services/cookie-reading.service';
+import { MatSnackBar } from '@angular/material';
+import { MapRoutingService } from '../../services/map-routing.service';
+import { APPCONFIG } from 'src/app/config';
 
 @Component({
   selector: 'ld-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  mapCookieName = APPCONFIG.mapCookieName;
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    public cookieReadingService: CookieReadingService,
+    private snackBar: MatSnackBar,
+    private mapRoutingService: MapRoutingService
+  ) { }
+
+  redirectToMap() {
+    this.snackBar.open('Redirecting, please wait...', 'Close', { duration: 60000 });
+
+    this.mapRoutingService
+      .getMapRouting()
+      .subscribe(route => this.document.location.href = route.route);
+  }
 }
